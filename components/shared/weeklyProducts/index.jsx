eaf85975e922407d406179d97bd56d8f53432033
HM-Azijul <azijulhm@gmail.com>
2022-10-26 09:04:37 +0600

    add reuse title component in WeeklyProducts section

---
+++ components/shared/weeklyProducts/index.jsx
@@ -0,0 +1,17 @@
+import Container from "../../UI/container";
+import Title from "../../UI/title";
+
+const WeeklyProducts = () => {
+  return (
+    <>
+      <Container>
+        <p>WeeklyProducts</p>
+        <Title content="Weekly Top 5 View Products" />
+
+        {/* TODO:add card => title, star, price, button reuse */}
+      </Container>
+    </>
+  );
+};
+
+export default WeeklyProducts;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import GlobalStyle from "../components/globalStyles";
 import { ThemeProvider } from "styled-components";
-import theme from "./theme";
+import theme from "../components/UI/theme";
+import WeeklyProducts from "../components/shared/weeklyProducts";
 
 export default function Home() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        <WeeklyProducts />
       </ThemeProvider>
     </>
   );
